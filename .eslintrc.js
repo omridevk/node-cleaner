6f4753903c0244f40c6fc47312289489dbfaa052
Omri Katz <omri.katz@kaltura.com>
2020-04-09 08:40:06 +0300

    fix lint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-  extends: 'erb/typescript',
-  rules: {
-    // A temporary hack related to IDE not resolving correct package.json
-    'import/no-extraneous-dependencies': 'off'
-  },
-  settings: {
-    'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
-      node: {},
-      webpack: {
-        config: require.resolve('./configs/webpack.config.eslint.js')
-      }
+    extends: ['prettier', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    plugins: ['react'],
+    rules: {
+        // A temporary hack related to IDE not resolving correct package.json
+        'import/no-extraneous-dependencies': 'off'
+    },
+    settings: {
+        'import/resolver': {
+            // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
+            node: {},
+            webpack: {
+                config: require.resolve('./configs/webpack.config.eslint.js')
+            }
+        }
     }
-  }
 };
